c37815e965b59318e18b2559fc3642686b9af252
Denis <corvus007@yandex.ru>
2017-06-02 16:12:23 +0300

    Изменяет размер стрелочек в слайдерах галереи и особенностей

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  var baseTemplateSlider = '<div class="fancybox-container" role="dialog" tabindex="-1">' +
+    '<div class="fancybox-bg"></div>' +
+    '<div class="fancybox-controls">' +
+      '<div class="fancybox-infobar">' +
+        '<div class="fancybox-infobar__body">' +
+          '<span class="js-fancybox-index"></span>&nbsp;/&nbsp;<span class="js-fancybox-count"></span>' +
+        '</div>' +
+      '</div>' +
+      '<div class="fancybox-buttons">' +
+        '<button data-fancybox-close class="fancybox-button fancybox-button--close" title="Close (Esc)"></button>' +
+      '</div>' +
+    '</div>' +
+    '<div class="fancybox-slider-wrap">' +
+      '<div class="fancybox-slider"></div>' +
+      '<button data-fancybox-previous class="fancybox-button fancybox-button--left" title="Previous"></button>' +
+      '<button data-fancybox-next class="fancybox-button fancybox-button--right" title="Next"></button>' +
+    '</div>' +
+    '<div class="fancybox-caption-wrap"><div class="fancybox-caption"></div></div>' +
+  '</div>';
+
   /*================================
   =            Main nav            =
   ================================*/
@@ -118,14 +138,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
   $('.gallery [data-fancybox]').fancybox({
     loop: false,
+    baseClass: 'gallery-popup',
+    baseTpl: baseTemplateSlider,
     afterMove: function(instance, slide) {
       $('.gallery-slider:visible').slick('slickGoTo', instance.currIndex - 1);
     }
   });
 
+  $('.features [data-fancybox]').fancybox({
+    baseClass: 'features-popup',
+    baseTpl: baseTemplateSlider,
+  });
+
   $('.reviews-slider').slick({
     accessibility: false,
     dots: true,
+    adaptiveHeight: true,
     infinite: false,
     responsive: [{
       breakpoint: 567,
